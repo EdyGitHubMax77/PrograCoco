2fe1244c46abbddae362fbf29f5aabefe8c630bf
EdyGitHubMax77 <esalazar@cocoschool.mx>
2020-04-21 14:50:07 -0500

    Tetris

---
+++ Tetris/Assets/Codigo/SpawnGameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGameOver : MonoBehaviour
+{
+    public GameObject[] Gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NewGameOver();
+    }
+
+    public void NewGameOver()
+    {
+        Instantiate(Gameover[Random.Range(0, Gameover.Length)], transform.position, Quaternion.identity);
+        
+    }
+}
